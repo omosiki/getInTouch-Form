52e97df39e7bc0fa66d0f056b505fc47f751d275
omosiki <abdullateefsikirat6@gmail.com>
2026-01-30 11:15:16 +0100

    finally fix the error

--- getInTouch.js
+++ getInTouch.js
@@ -1,15 +1,33 @@
 
+ const formY = document.querySelector("#myform")
+    
+document.addEventListener("DOMContentLoaded", ()=>{
+      if(!formY){
+        console.log("Form with id myform not Found")
+        return;
+      }
+    formY.addEventListener("submit", (event) =>{
+        event.preventDefault()
+     
 
-const consent = document.getElementById("consent")
-const btn = document.getElementById("submit-btn")
-btn.addEventListener("submit", function(e){
-    e.preventDefault()
-    const fullname = document.getElementById('fullname').value;
-const email = document.getElementById('email').value;
-const phone = document.getElementById("phone").value;
-const company = document.getElementById("company").value;
-const jobTittle = document.getElementById("job-title").value;
-const message = document.getElementById("message").value;
-const interest = document.getElementById("interest").value;
-console.log(fullname, email, phone, company, jobTittle, message, interest)
+            const fullname = document.getElementById('fullname').value;
+            const email = document.getElementById('email').value;
+            const phone = document.getElementById("phone").value;
+            const company = document.getElementById("company").value;
+            const jobTittle = document.getElementById("job-title").value;
+            const message = document.getElementById("message").value;
+            const interest = document.getElementById("interest").value;
+            console.log(fullname,
+                email,
+                phone,
+                company,
+                jobTittle, 
+                message,
+                interest)
+    })
 })
+
+
+
+// const consent = document.getElementById("consent")
+// const btn = document.getElementById("submit-btn")
